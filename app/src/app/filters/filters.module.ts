866471c34d090ddf00759e04689d0fbe7f16c05e
Anthony MOI <m.anthony.moi@gmail.com>
2018-02-12 01:42:04 +0100

    Number filter

--- app/src/app/filters/filters.module.ts
+++ app/src/app/filters/filters.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BytesPipe } from './bytes.pipe';
+import { NumberPipe } from './number.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ BytesPipe ],
-  exports: [ BytesPipe ]
+  declarations: [ BytesPipe, NumberPipe ],
+  exports: [ BytesPipe, NumberPipe ]
 })
 export class FiltersModule { }
---
+++ app/src/app/filters/number.pipe.ts
@@ -0,0 +1,37 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'number'
+})
+export class NumberPipe implements PipeTransform {
+  private pad(n: number) {
+    if (n < 1000 && n >= 100) {
+      return `${n}`;
+    } else if (n < 100 && n >= 10) {
+      return `0${n}`;
+    } else if (n < 10 && n >= 0) {
+      return `00${n}`;
+    }
+  }
+
+  transform(value: number): any {
+    if (typeof value === "string") {
+      value = parseFloat(value);
+    }
+    
+    const parts: string[] = [];
+    while (value > 1000) {
+      const remainer = value % 1000;
+      parts.unshift(this.pad(remainer));
+      value -= remainer;
+      value /= 1000;
+    }
+    
+    let text = `${value}`;
+    if (parts.length > 0) {
+      text += `,${parts.join(',')}`;
+    }
+    return text;
+  }
+
+}
