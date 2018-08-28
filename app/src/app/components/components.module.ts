a2b211f6c9eaf0f73c1067671b5258734eb30b75
Anthony MOI <m.anthony.moi@gmail.com>
2018-08-28 15:21:13 -0400

    pretty-json component

--- app/src/app/components/components.module.ts
+++ app/src/app/components/components.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PanelComponent } from './panel/panel.component';
+import { PrettyJsonComponent } from './pretty-json/pretty-json.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ PanelComponent ],
-  exports: [ PanelComponent ]
+  declarations: [ PanelComponent, PrettyJsonComponent ],
+  exports: [ PanelComponent, PrettyJsonComponent ]
 })
 export class ComponentsModule { }
