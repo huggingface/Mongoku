2d92544d4873a47f5e3d4650af9a58fc97ce4953
coyotte508 <coyotte508@protonmail.com>
2025-10-11 12:44:13 +0200

    promise streaming on collections page too

--- src/routes/servers/[server]/databases/[database]/collections/+page.server.ts
+++ src/routes/servers/[server]/databases/[database]/collections/+page.server.ts
@@ -16,11 +16,15 @@ export const load: PageServerLoad = async ({ params }) => {
 	const db = client.db(params.database);
 	const collections = await db.listCollections().toArray();
 
-	const collectionsJson = await Promise.all(collections.map((c) => getCollectionJson(db.collection(c.name), c.type)));
+	const collectionsWithDetails = collections.map((c) => ({
+		name: c.name,
+		type: c.type,
+		details: getCollectionJson(db.collection(c.name), c.type).catch(() => null),
+	}));
 
 	return {
 		server: params.server,
 		database: params.database,
-		collections: collectionsJson,
+		collections: collectionsWithDetails,
 	};
 };
--- src/routes/servers/[server]/databases/[database]/collections/+page.svelte
+++ src/routes/servers/[server]/databases/[database]/collections/+page.svelte
@@ -36,47 +36,65 @@
 							</a>
 						</td>
 						<td>
-							{#if collection.count !== undefined}
-								{formatNumber(collection.count)}
-							{/if}
+							{#await collection.details}
+								<span class="text-gray-400">...</span>
+							{:then details}
+								{#if details}
+									{formatNumber(details.count)}
+								{:else}
+									<span title="Failed to load">❌</span>
+								{/if}
+							{/await}
 						</td>
 						<td>
-							{#if collection.nIndexes !== undefined}
-								<TooltipTable
-									columns={[
-										{ header: "Index", key: "definition", align: "left" },
-										{ header: "Size", key: "size", align: "right" },
-									]}
-									rows={(collection.indexes || []).map((index) => ({
-										definition: index.key ? JSON.stringify(index.key, null, 1) : index.name,
-										size: formatBytes(index.size),
-									}))}
-								>
-									{formatNumber(collection.nIndexes)}
-								</TooltipTable>
-							{/if}
+							{#await collection.details}
+								<span class="text-gray-400">...</span>
+							{:then details}
+								{#if details}
+									<TooltipTable
+										columns={[
+											{ header: "Index", key: "definition", align: "left" },
+											{ header: "Size", key: "size", align: "right" },
+										]}
+										rows={details.indexes.map((index) => ({
+											definition: index.key ? JSON.stringify(index.key, null, 1) : index.name,
+											size: formatBytes(index.size),
+										}))}
+									>
+										{formatNumber(details.nIndexes)}
+									</TooltipTable>
+								{:else}
+									<span title="Failed to load">❌</span>
+								{/if}
+							{/await}
 						</td>
 						<td>
-							{#if collection.size !== undefined}
-								<TooltipTable
-									hideHeader
-									columns={[
-										{ header: "Label", key: "label", align: "left" },
-										{ header: "Value", key: "value", align: "right" },
-									]}
-									rows={[
-										{ label: "Average obj. size", value: collection.avgObjSize },
-										{ label: "Data size", value: collection.dataSize },
-										{ label: "Storage size", value: collection.storageSize },
-										{ label: "Index size", value: collection.totalIndexSize },
-									].map((row) => ({
-										...row,
-										value: typeof row.value === "number" ? formatBytes(row.value) : row.value,
-									}))}
-								>
-									{formatBytes(collection.size)}
-								</TooltipTable>
-							{/if}
+							{#await collection.details}
+								<span class="text-gray-400">...</span>
+							{:then details}
+								{#if details}
+									<TooltipTable
+										hideHeader
+										columns={[
+											{ header: "Label", key: "label", align: "left" },
+											{ header: "Value", key: "value", align: "right" },
+										]}
+										rows={[
+											{ label: "Average obj. size", value: details.avgObjSize },
+											{ label: "Data size", value: details.dataSize },
+											{ label: "Storage size", value: details.storageSize },
+											{ label: "Index size", value: details.totalIndexSize },
+										].map((row) => ({
+											...row,
+											value: typeof row.value === "number" ? formatBytes(row.value) : row.value,
+										}))}
+									>
+										{formatBytes(details.size)}
+									</TooltipTable>
+								{:else}
+									<span title="Failed to load">❌</span>
+								{/if}
+							{/await}
 						</td>
 					</tr>
 				{/each}
