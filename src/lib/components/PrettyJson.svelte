1440bffce8762f696afec452133d70fcc4973165
coyotte508 <coyotte508@protonmail.com>
2025-10-29 13:07:41 +0100

    fix editing doc with overlay

--- src/lib/components/PrettyJson.svelte
+++ src/lib/components/PrettyJson.svelte
@@ -242,8 +242,8 @@
 {/snippet}
 
 <Panel class="group" title={json._id ? title : undefined} {actions}>
-	<div bind:this={contentContainerRef} class="p-4 relative border-t border-[var(--border-color)] overflow-x-auto">
-		<div class="font-mono text-sm leading-tight whitespace-pre-wrap break-words relative">
+	<div bind:this={contentContainerRef} class=" relative border-t border-[var(--border-color)]">
+		<div class="font-mono text-sm leading-tight whitespace-pre-wrap break-words p-4 overflow-x-auto">
 			<JsonValue value={json} {autoCollapse} collapsed={false} {isKeyMapped} {fetchMappedDocument} />
 		</div>
 
