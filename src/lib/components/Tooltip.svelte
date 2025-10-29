90dcf7ba9315cdd2c33c50c376976cd756cf4888
coyotte508 <coyotte508@protonmail.com>
2025-10-29 13:16:29 +0100

    tooltip

--- src/lib/components/Tooltip.svelte
+++ src/lib/components/Tooltip.svelte
@@ -70,11 +70,12 @@
 <div class="relative inline-block" bind:this={containerElement}>
 	{@render trigger()}{#if show}
 		<div
-			class="fixed bg-[var(--light-background)] border border-[var(--color-3)] rounded z-[1000] shadow-lg {tooltipClass}"
+			class="fixed bg-[var(--light-background)] border border-[var(--border-color)] rounded-2xl z-[1000] shadow-xl backdrop-blur-md {tooltipClass}"
 			bind:this={tooltipElement}
 			style:left={tooltipPosition.left}
 			style:right={tooltipPosition.right}
 			style:top={tooltipPosition.top}
+			style="color: var(--text);"
 		>
 			{@render content()}
 		</div>
